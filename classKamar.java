e451af85c9d7f2277834a5c8d7c495b860478c68
abdHalidSuma18 <85104994+abdHalidSuma18@users.noreply.github.com>
2021-05-31 21:59:40 -0700

    menambahkan nama pada komentar program

--- classKamar.java
+++ classKamar.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//Dandi Septian Laminullah
+//Program oop penyewaan Kos
 package com.java.SewaKOS.model;
 
 /**
